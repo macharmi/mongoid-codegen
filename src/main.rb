0f0dda7f0d6cb2b211465e2c5ee8457db08d805f
MedAli CHARMI <charmi@live.com>
2015-01-12 21:33:10 +0100

    Add some directories to the final package

--- src/main.rb
+++ src/main.rb
@@ -11,7 +11,11 @@ end
 ARGV[1].to_s == "" ? location= "./" : location = ARGV[1] + '/'
 !Dir.exists?(location + "models") ? Dir.mkdir(location + "models") : nil
 !Dir.exists?(location + "views") ? Dir.mkdir(location + "views") : nil
-
+!Dir.exists?(location + "views/layout") ? Dir.mkdir(location + "views/layout") : nil
+!Dir.exists?(location + "config") ? Dir.mkdir(location + "config") : nil
+!Dir.exists?(location + "public") ? Dir.mkdir(location + "public") : nil
+!Dir.exists?(location + "public/css") ? Dir.mkdir(location + "public/css") : nil
+!Dir.exists?(location + "public/js") ? Dir.mkdir(location + "public/js") : nil
 
 # open file
 begin
@@ -21,16 +25,23 @@ rescue
     exit
 end
 
-# parse json file
+# parse JSON file
 begin
 specs = JSON.parse(file)
 rescue
-    puts "Error: json file is invalid"
+    puts "Error: JSON file is invalid"
     exit
 end
 
 # browse json structure and generate classes code
 specs['documents'].each{|doc|
+    # create model class
     File.write(location + "models/" + doc['name'].downcase + '.rb', generate_class(doc))
+    
+    # create view folder
+    !Dir.exists?(location + "views/" + doc['name'].downcase) ? Dir.mkdir(location + "views/" + doc['name'].downcase) : nil
+    
+    # create html views
+    
 }
 
